0c0a105b2547e9570cd48a4b14c6299d779b6cbf
annusgit <annusgit@gmail.com>
2020-12-21 22:49:39 +0500

    add 3

--- LandCoverSegmentation/semantic_segmentation/dataset.py
+++ LandCoverSegmentation/semantic_segmentation/dataset.py
@@ -609,6 +609,10 @@ def get_dataloaders_generated_data(generated_data_path, save_data_path, model_in
     test_data = dataset(data_list=test_list, data_map_path=os.path.join(save_data_path, 'test_datamap.pkl'), mode='test', stride=model_input_size,
                         transformation=transformation)
     print('LOG: [train_data, eval_data, test_data] ->', len(train_data), len(eval_data), len(test_data))
+    print('LOG: [train_list, eval_list, test_list] ->', len(train_list), len(eval_list), len(test_list))
+    print('LOG: set(train_list).isdisjoint(set(eval_list)) ->', set(train_list).isdisjoint(set(eval_list)))
+    print('LOG: set(train_list).isdisjoint(set(test_list)) ->', set(train_list).isdisjoint(set(test_list)))
+    print('LOG: set(test_list).isdisjoint(set(eval_list)) ->', set(test_list).isdisjoint(set(eval_list)))
 
     # for j in range(10):
     #     print(train_data[j])

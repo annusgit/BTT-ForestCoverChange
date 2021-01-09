6ce5d5d2d9392faad7bdd0c5734bee72076d889f
annusgit <annusgit@gmail.com>
2021-01-09 18:16:51 +0500

    F1-score for full-spectrum model

--- Statistical_Classifiers/confusion_matrix_and_f1_score.py
+++ Statistical_Classifiers/confusion_matrix_and_f1_score.py
@@ -1,20 +1,16 @@
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.datasets import load_wine
+from sklearn.svm import SVC
 
 
 def main():
     dataset = load_wine()
-    X = dataset.data
-    y = dataset.target
-    from sklearn.model_selection import train_test_split
-    from sklearn.svm import SVC
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    X, Y = dataset.data, dataset.target
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state=0)
     svc = SVC(kernel='rbf', C=1).fit(X_train, y_train)
     y_pred = svc.predict(X_test)
-
     confusion = confusion_matrix(y_test, y_pred)
     print('Confusion Matrix\n')
     print(confusion)
---
+++ Statistical_Classifiers/statistical_models_tests.py
@@ -0,0 +1,36 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.datasets import load_wine
+
+# all the classifiers we want to try here
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+def run(classifier):
+    # load a test dataset and split it
+    dataset = load_wine()
+    X, Y = dataset.data, dataset.target
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state=0)
+    # fit the model on your dataset
+    trained_classifier = classifier.fit(X_train, y_train)
+    # get predictions on unseen data
+    y_pred = trained_classifier.predict(X_test)
+    # get confusion matrix
+    confusion_matrix_to_print = confusion_matrix(y_test, y_pred)
+    # show confusion matrix and classification report for precision, recall, F1-score
+    print('Confusion Matrix\n')
+    print(confusion_matrix_to_print)
+    print('\nClassification Report\n')
+    print(classification_report(y_test, y_pred, target_names=['Null-Pixels', 'Non-Forest', 'Forest']))
+    pass
+
+
+if __name__ == "__main__":
+    # get your model (RandomForestClassifier, DecisionTreeClassifier, SVC, GaussianNB, LogisticRegression, Perceptron)
+    run(classifier=Perceptron())

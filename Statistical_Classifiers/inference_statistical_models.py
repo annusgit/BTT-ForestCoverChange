f3d3e1b4dbc9dfa6104d7182c397ec8ccc97b031
annusgit <annusgit@gmail.com>
2021-01-11 03:54:17 +0500

    statistical models

--- Statistical_Classifiers/inference_statistical_models.py
+++ Statistical_Classifiers/inference_statistical_models.py
@@ -135,7 +135,7 @@ def run_inference(args):
             generated_map = np.empty(shape=inference_loader.dataset.get_image_size())
             for idx, data in enumerate(inference_loader):
                 coordinates, test_x = data['coordinates'].tolist(), data['input']
-                # print(test_x.shape)
+                print(test_x.shape)
                 # test_x = test_x.numpy().transpose(1,2,0)
                 pred_numpy = trained_classifier.predict(test_x.reshape(-1, 11)).reshape((64, 128, 128))
                 if idx % 5 == 0:

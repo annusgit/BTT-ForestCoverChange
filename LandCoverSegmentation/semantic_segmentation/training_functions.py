aa1fdf2cc81ff08cbe608883322af19cb50ed239
annusgit <annusgit@gmail.com>
2021-01-17 16:20:26 +0500

    converted to binary classification problem

--- LandCoverSegmentation/semantic_segmentation/training_functions.py
+++ LandCoverSegmentation/semantic_segmentation/training_functions.py
@@ -195,7 +195,7 @@ def eval_net(**kwargs):
         print('Confusion Matrix from Scikit-Learn\n')
         print(confusion)
         print('\nClassification Report\n')
-        print(classification_report(all_ground_truth, all_predictions, target_names=['Null-Pixels', 'Non-Forest', 'Forest']))
+        print(classification_report(all_ground_truth, all_predictions, target_names=['Non-Forest', 'Forest']))
         print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
         return mean_accuracy
 
@@ -268,7 +268,7 @@ def eval_net(**kwargs):
         print('Confusion Matrix from Scikit-Learn\n')
         print(confusion)
         print('\nClassification Report\n')
-        print(classification_report(all_ground_truth, all_predictions, target_names=['Null-Pixels', 'Non-Forest', 'Forest']))
+        print(classification_report(all_ground_truth, all_predictions, target_names=['Non-Forest', 'Forest']))
         with open('normalized.pkl', 'wb') as this:
             pkl.dump(confusion_meter.value(), this, protocol=pkl.HIGHEST_PROTOCOL)
         with open('un_normalized.pkl', 'wb') as this:

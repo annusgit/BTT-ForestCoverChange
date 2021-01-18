30b37fa6e79989055279787166cf3b65bc1b3d02
annusgit <annusgit@gmail.com>
2021-01-18 18:03:45 +0500

    eval fix

--- LandCoverSegmentation/semantic_segmentation/training_functions.py
+++ LandCoverSegmentation/semantic_segmentation/training_functions.py
@@ -157,7 +157,10 @@ def eval_net(**kwargs):
             not_one_hot_target = torch.argmax(label, dim=1)
             # dice_criterion(softmaxed, label) # + focal_criterion(softmaxed, not_one_hot_target) #
             # loss = crossentropy_criterion(softmaxed.view(-1, 2), label.view(-1, 2))
-            loss = focal_criterion(softmaxed, not_one_hot_target-1)  # dice_criterion(softmaxed, label) #
+            not_one_hot_target_for_loss = not_one_hot_target
+            not_one_hot_target_for_loss[not_one_hot_target_for_loss == 0] = 1
+            not_one_hot_target_for_loss -= 1
+            loss = focal_criterion(softmaxed, not_one_hot_target_for_loss)  # dice_criterion(softmaxed, label) #
             label_valid_indices = (not_one_hot_target.view(-1) != 0)
             # mind the '-1' fix please. This is to convert Forest and Non-Forest labels from 1, 2 to 0, 1
             valid_label = not_one_hot_target.view(-1)[label_valid_indices] - 1
@@ -229,7 +232,10 @@ def eval_net(**kwargs):
             pred = torch.argmax(softmaxed, dim=1)
             not_one_hot_target = torch.argmax(label, dim=1)
             #######################################################
-            loss = focal_criterion(softmaxed, not_one_hot_target-1)  # dice_criterion(softmaxed, label) #
+            not_one_hot_target_for_loss = not_one_hot_target
+            not_one_hot_target_for_loss[not_one_hot_target_for_loss == 0] = 1
+            not_one_hot_target_for_loss -= 1
+            loss = focal_criterion(softmaxed, not_one_hot_target_for_loss)  # dice_criterion(softmaxed, label) #
             label_valid_indices = (not_one_hot_target.view(-1) != 0)
             # mind the '-1' fix please. This is to convert Forest and Non-Forest labels from 1, 2 to 0, 1
             valid_label = not_one_hot_target.view(-1)[label_valid_indices] - 1
